a81ed9d538d85acfd0150e94dccf56cbe0b10236
Valentino Christian <valentinocc@ufl.edu>
2019-07-09 16:46:17 -0400

    added picture model, made migrations

--- breed_identifier/admin.py
+++ breed_identifier/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Dog_Picture
 
-# Register your models here.
+admin.site.register(Dog_Picture)
--- breed_identifier/models.py
+++ breed_identifier/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Dog_Picture(models.Model):
+	image = models.ImageField(default='default.jpg', upload_to='dog_pics')
